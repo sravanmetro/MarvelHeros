5878fe4cb5b5836b1bd63644041d9ecaf24656cc
Sravan <sravan.pabolu@metrobank.plc.uk>
2025-07-16 11:27:23 +0100

    Initial Commit

---
+++ MarvelHeros/MarvelHerosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHerosApp.swift
+//  MarvelHeros
+//
+//  Created by Sravan on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHerosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarvelHerosTests/MarvelHerosTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHerosTests.swift
+//  MarvelHerosTests
+//
+//  Created by Sravan on 16/07/2025.
+//
+
+import Testing
+@testable import MarvelHeros
+
+struct MarvelHerosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
